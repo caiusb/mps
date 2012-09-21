a24cb068400ffa0451ea87d9f4d7369a7386461b
Caius Brindescu <caius.brindescu@gmail.com>
2012-09-21 17:21:53 -0500

    Copied the unsynchronized implementation
    Now, I only have to add synchronization to the right place.

--- src/primes/PrimesLiveSync.java
+++ src/primes/PrimesLiveSync.java
@@ -1,5 +1,9 @@
 package primes;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 import util.UnimplementedExercise;
 
 /* 
@@ -13,22 +17,75 @@ import util.UnimplementedExercise;
 public class PrimesLiveSync extends PrimesComputation implements
 		LiveResults<Integer[]>, UnimplementedExercise {
 
+	private Integer[] liveResults;
+	private int count = 0;
+
 	@Override
 	public Boolean[] computePrimes(int upto) {
-		// TODO: implement this method
-		return null;
+		liveResults = new Integer[upto];
+
+		int noOfCores = Runtime.getRuntime().availableProcessors();
+		int chunkSize = 2000;
+		int tasks = upto / chunkSize;
+		Boolean[] results = new Boolean[upto];
+
+		ExecutorService threadPool = Executors.newFixedThreadPool(noOfCores);
+
+		for (int i = 0; i < tasks; i++)
+			threadPool.execute(new PrimesApproximationTask(results, i
+					* chunkSize, (i + 1) * chunkSize));
+
+		threadPool.shutdown();
+		try {
+			while (!threadPool.awaitTermination(500, TimeUnit.MILLISECONDS))
+				;
+		} catch (InterruptedException e) {
+		}
+
+		return results;
 	}
 
+	class PrimesApproximationTask implements Runnable {
+
+		private Boolean[] results;
+		private int lo;
+		private int hi;
+
+		public PrimesApproximationTask(Boolean[] results, int lo, int hi) {
+			this.results = results;
+			this.lo = lo;
+			this.hi = hi;
+
+		}
+
+		@Override
+		public void run() {
+			for (int x = lo; x < hi; x++) {
+				results[x] = isPrime(x);
+				if (results[x])
+					liveResults[count++] = x;
+			}
+		}
+	}
+
+	/**
+	 * Returns an array of prime numbers. Each getResults() invocation returns
+	 * the same array with increasingly larger number of cells replaced by prime
+	 * numbers, as the computation progresses. This method can be invoked at any
+	 * time from outside the PrimesLive object.
+	 */
 	@Override
 	public Integer[] getPrimes() {
-		// TODO implement this method
-		return null;
+		return liveResults;
 	}
 
+	/**
+	 * Returns the number of primes that have been computed so far. This method
+	 * can be invoked at any time from outside the PrimesLive object.
+	 */
 	@Override
 	public int primesCount() {
-		// TODO implement this method
-		return 0;
+		return count;
 	}
 
 }

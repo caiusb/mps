d1dae5a50ef62ed642082575a1223cc9980dcf4c
Caius Brindescu <caius.brindescu@gmail.com>
2012-09-21 19:23:31 -0500

    Removed unused imports & format
    Just like the title

--- src/pi/PiLive.java
+++ src/pi/PiLive.java
@@ -2,9 +2,6 @@ package pi;
 
 import java.util.Random;
 
-import pi.PiThreads.PiApproximationThread;
-import util.UnimplementedExercise;
-
 /**
  * Parallel implementation of {@link PiApproximation} that also reports the
  * approximation so far using the {@link #liveValue()} method.
@@ -16,28 +13,28 @@ import util.UnimplementedExercise;
 
 /*
  * 
- *  In this exercise, we make the Pi example slightly more complex. 
- 	Let's say we want a client of our class to be able to observe the most 
- 	up-to-date approximation of pi as the computation progresses. 
-
-	Steps:
-
- 1. We start from the threads version of this exercise. Replace the computePi
-  	below with the computePi and the companion inner class from either 
-  	your or the given threads solution.
-
- 2. Gather approximate results from the threads in a shared data structure.
-	Make it possible for other concurrent threads to inspect the most up to date 
-	value by calling the liveValue() method.
-	
- 3. Run PiTest. You might notice that the live approximation is slightly skewed. 
-    This is not only because the computation is not over, but also because of 
-    race conditions. Do not add synchronization here. The next exercise (PiLiveSync) 
-    will ask you to introduce synchronization.
-*/
+ * In this exercise, we make the Pi example slightly more complex. Let's say we
+ * want a client of our class to be able to observe the most up-to-date
+ * approximation of pi as the computation progresses.
+ * 
+ * Steps:
+ * 
+ * 1. We start from the threads version of this exercise. Replace the computePi
+ * below with the computePi and the companion inner class from either your or
+ * the given threads solution.
+ * 
+ * 2. Gather approximate results from the threads in a shared data structure.
+ * Make it possible for other concurrent threads to inspect the most up to date
+ * value by calling the liveValue() method.
+ * 
+ * 3. Run PiTest. You might notice that the live approximation is slightly
+ * skewed. This is not only because the computation is not over, but also
+ * because of race conditions. Do not add synchronization here. The next
+ * exercise (PiLiveSync) will ask you to introduce synchronization.
+ */
 
 public class PiLive implements PiApproximation, LiveValue {
-	
+
 	private PiApproximationThread[] threads;
 	private int noOfCores;
 
@@ -45,28 +42,28 @@ public class PiLive implements PiApproximation, LiveValue {
 	public double computePi(long iterations) {
 		noOfCores = Runtime.getRuntime().availableProcessors();
 		threads = new PiApproximationThread[noOfCores];
-		
-		long partialIterations = iterations/noOfCores;
-		for (int i=0; i<noOfCores; i++) {
+
+		long partialIterations = iterations / noOfCores;
+		for (int i = 0; i < noOfCores; i++) {
 			threads[i] = new PiApproximationThread(partialIterations);
 			threads[i].start();
 		}
-		
-		for (int i=0; i<noOfCores; i++)
+
+		for (int i = 0; i < noOfCores; i++)
 			try {
 				threads[i].join();
 			} catch (InterruptedException e) {
 			}
-		
+
 		double partialResult = 0;
-		for (int i=0; i<noOfCores; i++)
+		for (int i = 0; i < noOfCores; i++)
 			partialResult += threads[i].getResult();
-		
-		return partialResult/(double)noOfCores;
+
+		return partialResult / (double) noOfCores;
 	}
 
 	class PiApproximationThread extends Thread {
-		
+
 		private double partialPi = 0;
 		private final long iterations;
 		private long inside = 0;
@@ -75,11 +72,11 @@ public class PiLive implements PiApproximation, LiveValue {
 		public PiApproximationThread(long iterations) {
 			this.iterations = iterations;
 		}
-		
+
 		public double getResult() {
 			return partialPi;
 		}
-		
+
 		public double getPartial() {
 			return ((double) inside) / soFar * 4;
 		}
@@ -88,7 +85,7 @@ public class PiLive implements PiApproximation, LiveValue {
 		public void run() {
 			inside = 0;
 			Random randomNumberGenerator = new Random();
-			for (soFar  = 0; soFar  < iterations; soFar ++) {
+			for (soFar = 0; soFar < iterations; soFar++) {
 				double x = randomNumberGenerator.nextDouble();
 				double y = randomNumberGenerator.nextDouble();
 				double lenght = x * x + y * y;
@@ -100,8 +97,8 @@ public class PiLive implements PiApproximation, LiveValue {
 	}
 
 	/**
-	 * Return the most up-to-date Pi approximation. This method can be invoked at any time
-	 * from outside the PiLive object.
+	 * Return the most up-to-date Pi approximation. This method can be invoked
+	 * at any time from outside the PiLive object.
 	 */
 	public double liveValue() {
 		double partialResult = 0;

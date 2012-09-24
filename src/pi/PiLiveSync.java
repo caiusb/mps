7cc0da5c21729f1eb809acae4b3fadea2d42e365
Caius Brindescu <caius.brindescu@gmail.com>
2012-09-24 10:57:08 -0500

    Copied the implementation from PiLive
    Now just to add synchronization.

--- src/pi/PiLiveSync.java
+++ src/pi/PiLiveSync.java
@@ -1,5 +1,7 @@
 package pi;
 
+import java.util.Random;
+
 import util.UnimplementedExercise;
 
 /**
@@ -12,23 +14,86 @@ import util.UnimplementedExercise;
  */
 
 /*
- Steps:
- 
- 1. Copy your PiLive solution to the class below.
-
- 2. Fix the races using synchronization.
+ * Steps:
+ * 
+ * 1. Copy your PiLive solution to the class below.
+ * 
+ * 2. Fix the races using synchronization.
  */
 
 public class PiLiveSync implements PiApproximation, UnimplementedExercise {
 
+	private PiApproximationThread[] threads;
+	private int noOfCores;
+
 	@Override
 	public double computePi(long iterations) {
-		// TODO: implement
-		return 100.0;
+		noOfCores = Runtime.getRuntime().availableProcessors();
+		threads = new PiApproximationThread[noOfCores];
+
+		long partialIterations = iterations / noOfCores;
+		for (int i = 0; i < noOfCores; i++) {
+			threads[i] = new PiApproximationThread(partialIterations);
+			threads[i].start();
+		}
+
+		for (int i = 0; i < noOfCores; i++)
+			try {
+				threads[i].join();
+			} catch (InterruptedException e) {
+			}
+
+		double partialResult = 0;
+		for (int i = 0; i < noOfCores; i++)
+			partialResult += threads[i].getResult();
+
+		return partialResult / (double) noOfCores;
 	}
 
+	class PiApproximationThread extends Thread {
+
+		private double partialPi = 0;
+		private final long iterations;
+		private long inside = 0;
+		private int soFar = 0;
+
+		public PiApproximationThread(long iterations) {
+			this.iterations = iterations;
+		}
+
+		public double getResult() {
+			return partialPi;
+		}
+
+		public double getPartial() {
+			return ((double) inside) / soFar * 4;
+		}
+
+		@Override
+		public void run() {
+			inside = 0;
+			Random randomNumberGenerator = new Random();
+			for (soFar = 0; soFar < iterations; soFar++) {
+				double x = randomNumberGenerator.nextDouble();
+				double y = randomNumberGenerator.nextDouble();
+				double lenght = x * x + y * y;
+				if (lenght < 1.0)
+					inside++;
+			}
+			partialPi = ((double) inside) / iterations * 4;
+		}
+	}
+
+	/**
+	 * Return the most up-to-date Pi approximation. This method can be invoked
+	 * at any time from outside the PiLive object.
+	 */
 	public double liveValue() {
-		// TODO: implement
-		return 100.0;
+		double partialResult = 0;
+		for (PiApproximationThread thread : threads) {
+			partialResult += thread.getPartial();
+		}
+
+		return partialResult / noOfCores;
 	}
 }

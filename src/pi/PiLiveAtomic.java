c5aaca74e4ef7edee0a62ce111e4bae0d1adf46e
Caius Brindescu <caius.brindescu@gmail.com>
2012-10-01 17:55:42 -0500

    The LiveAtomic implementation
    It sucks because of the high contention

--- src/pi/PiLiveAtomic.java
+++ src/pi/PiLiveAtomic.java
@@ -1,6 +1,7 @@
 package pi;
 
-import util.UnimplementedExercise;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Parallel implementation of {@link PiApproximation} that also reports the
@@ -12,23 +13,72 @@ import util.UnimplementedExercise;
  */
 
 /*
- Steps:
-	
- 1. Copy your or the given solution to PiLive to the class below.
-
- 2. Fix the races using an Atomic construct.
+ * Steps:
+ * 
+ * 1. Copy your or the given solution to PiLive to the class below.
+ * 
+ * 2. Fix the races using an Atomic construct.
  */
 
-public class PiLiveAtomic implements PiApproximation, UnimplementedExercise {
+public class PiLiveAtomic implements PiApproximation, LiveValue {
+
+	// update the live value each UPDATE_EACH cycles
+	//public static final int UPDATE_EACH = 100;
+
+	AtomicLong globalInside = new AtomicLong(0);
+	AtomicLong globalTotal = new AtomicLong(0);
 
 	@Override
-	public double computePi(long iterations) {
-		// TODO: implement
-		return 100.0;
+	public double computePi(long iterations) throws InterruptedException {
+		int noProcessors = Runtime.getRuntime().availableProcessors();
+		PiApproximationThread[] threads = new PiApproximationThread[noProcessors];
+
+		for (int i = 0; i < noProcessors; i++) {
+			threads[i] = new PiApproximationThread(iterations / noProcessors);
+			threads[i].start();
+		}
+
+		double pi = 0;
+
+		for (int i = 0; i < noProcessors; i++) {
+			threads[i].join();
+			pi += threads[i].result() / noProcessors;
+		}
+		return pi;
 	}
 
 	public double liveValue() {
-		// TODO: implement
-		return 100.0;
+		return globalInside.get() * 4.0 / globalTotal.get();
+	}
+
+	class PiApproximationThread extends Thread {
+		private double pi;
+		private final long iterations;
+
+		public PiApproximationThread(long iterations) {
+			this.iterations = iterations;
+		}
+
+		@Override
+		public void run() {
+			long inside = 0;
+			Random rand = new Random();
+
+			for (int i = 0; i < iterations; i++) {
+				double x = rand.nextDouble();
+				double y = rand.nextDouble();
+				double lenght = x * x + y * y;
+				if (lenght < 1.0) {
+					globalInside.incrementAndGet();
+					inside++;
+				}
+				globalTotal.incrementAndGet();
+			}
+			pi = inside * 4.0 / iterations;
+		}
+
+		public double result() {
+			return pi;
+		}
 	}
 }
--- src/primes/PrimesLiveAtomic.java
+++ src/primes/PrimesLiveAtomic.java
@@ -1,35 +1,92 @@
 package primes;
 
-import util.UnimplementedExercise;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /* 
  Steps:
 
  1. Copy your or the given solution to PrimesLive to the class below. We keep the 
- 	original so that we can compare results.
+ original so that we can compare results.
 
  2. Fix the races by replacing the array index with an AtomicInteger.
  */
 
 public class PrimesLiveAtomic extends PrimesComputation implements
-		LiveResults<Integer[]>, UnimplementedExercise {
+		LiveResults<Integer[]> {
+
+	private Integer[] liveResults;
+	private AtomicInteger count = new AtomicInteger();
 
 	@Override
 	public Boolean[] computePrimes(int upto) {
-		// TODO: implement this method
-		return null;
+		liveResults = new Integer[upto];
+		count.set(0);
+
+		int noOfCores = Runtime.getRuntime().availableProcessors();
+		int chunkSize = 2000;
+		int tasks = upto / chunkSize;
+		Boolean[] results = new Boolean[upto];
+
+		ExecutorService threadPool = Executors.newFixedThreadPool(noOfCores);
+
+		for (int i = 0; i < tasks; i++)
+			threadPool.execute(new PrimesApproximationTask(results, i
+					* chunkSize, (i + 1) * chunkSize));
+
+		threadPool.shutdown();
+		try {
+			while (!threadPool.awaitTermination(500, TimeUnit.MILLISECONDS))
+				;
+		} catch (InterruptedException e) {
+		}
+
+		return results;
 	}
 
+	class PrimesApproximationTask implements Runnable {
+
+		private Boolean[] results;
+		private int lo;
+		private int hi;
+
+		public PrimesApproximationTask(Boolean[] results, int lo, int hi) {
+			this.results = results;
+			this.lo = lo;
+			this.hi = hi;
+
+		}
+
+		@Override
+		public void run() {
+			for (int x = lo; x < hi; x++) {
+				results[x] = isPrime(x);
+				if (results[x])
+					liveResults[count.getAndIncrement()] = x;
+			}
+		}
+	}
+
+	/**
+	 * Returns an array of prime numbers. Each getResults() invocation returns
+	 * the same array with increasingly larger number of cells replaced by prime
+	 * numbers, as the computation progresses. This method can be invoked at any
+	 * time from outside the PrimesLive object.
+	 */
 	@Override
 	public Integer[] getPrimes() {
-		// TODO implement this method
-		return null;
+		return liveResults;
 	}
 
+	/**
+	 * Returns the number of primes that have been computed so far. This method
+	 * can be invoked at any time from outside the PrimesLive object.
+	 */
 	@Override
 	public int primesCount() {
-		// TODO implement this method
-		return 0;
+		return count.get();
 	}
 
 }
